3b6b8da081ace503d849b4975fe061a01cfda0e2
John Adibe <johnadibe450@gmail.com>
2021-10-24 03:40:37 +0100

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -32,19 +32,19 @@ const Navbar = () => {
       <div className="right-nav">
         <ul>
           <li>
-            <a href="#home">Home.</a>
+            <a href="#home">Home</a>
           </li>
           <li>
-            <a href="#about">About.</a>
+            <a href="#about">About</a>
           </li>
           <li>
-            <a href="#experience">Experience.</a>
+            <a href="#experience">Experience</a>
           </li>
           <li>
-            <a href="#projects">Projects.</a>
+            <a href="#projects">Projects</a>
           </li>
           <li>
-            <a href="#contact">Contact.</a>
+            <a href="#contact">Contact</a>
           </li>
         </ul>
       </div>
