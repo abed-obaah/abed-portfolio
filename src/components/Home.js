70707b8de6ee4cc42034c9ddf104c4666abb2e01
johnphealipto <johnadibe450@gmail.com>
2021-11-19 02:22:06 -0800

    Update on Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,6 +10,16 @@ const Home = () => {
         <h1 className="my-name">John Adibe</h1>
         <h1>Frontend Developer</h1>
         <p>Focused on building accessible & human-centered products.</p>
+        <div className="contact-details">
+          <div>
+            <p>Email:</p>
+            <a href="mailto:johnadibe450@gmail.com">johnadibe450@gmail.com</a>
+          </div>
+          <div>
+            <p>Mobile:</p>
+            <a href="tel:+2348141726099">+234-81-4172-6099</a>
+          </div>
+        </div>
         <ul className="social-connect">
           <li>
             <a href="https://github.com/johnphealipto" target="_blank">
