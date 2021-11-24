b1baaf7bbb3761a6a51452de00c78d2391ef65fe
John Adibe <johnadibe450@gmail.com>
2021-11-24 20:04:17 +0100

    About section :zpa:

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,8 +3,22 @@ import React from 'react';
 const About = () => {
   return (
     <section id="about">
-      <div className="about-content">
-        <h1>About Me</h1>
+      <div className="section-content">
+        <h2 className="jae_title">About Me</h2>
+        <div className="about-details">
+          <p>My name is John Adibe Ebube. I am a Frontend Developer settled in Lagos State, Nigeria who derives passion from modern Tech innovations.</p>
+          <p>I started as a Maths Tutor, thanks to my degree in Mathematics. Tutoring & mentoring for about 3 years led to an idea of boosting my knowledge on Tech - then I saw myself building Web-based & Mobile-based app.</p>
+          <p>Today, I am currently working as a Software Developer at <a href="https://www.outcess.com/" className="a_link" target="_blank">Outcess Solutions LTD</a> where I build enhanced solution provided applications.</p>
+          <p>Below are the few technologies I work with:</p>
+          <ul>
+            <li><span>JavaScript</span></li>
+            <li><span>Redux</span></li>
+            <li><span>React.JS</span></li>
+            <li><span>GraphQL</span></li>
+            <li><span>React Native</span></li>
+            <li><span>Node</span></li>
+          </ul>
+        </div>
       </div>
     </section>
   )
