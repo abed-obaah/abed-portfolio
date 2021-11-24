7c226a1320e42b2dd2d648b085a39e6200e4c1b9
John Adibe <johnadibe450@gmail.com>
2021-11-25 00:31:38 +0100

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
       <div className="section-content">
         <h2 className="jae_title">About Me</h2>
         <div className="about-details">
-          <pI am a Frontend Developer based in Lagos, Nigeria who derives passion from modern Tech innovations.</p>
+          <p>I am a Frontend Developer based in Lagos, Nigeria who derives passion from modern Tech innovations.</p>
           <p>I started off as a Maths Tutor, thanks to my degree in Mathematics. Tutored for about 3 years, then after - as a Tech enthusiast, I choose to explore more on its fundamentals. Fast-forward to this moment, I now can build web-based & mobile-based applications.</p>
           <p>Today, I am currently working as a Software Developer at <a href="https://www.outcess.com/" className="a_link" target="_blank">Outcess Solutions LTD</a> where I build enhanced solution provided applications.</p>
           <p>Below are the few technologies I work with:</p>
