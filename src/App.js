4bbb148530b832542d061307321fe2f7bf23bcab
John Adibe <johnadibe450@gmail.com>
2021-12-03 00:00:50 +0100

    Mobile nav :zap:

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Navbar from './components/Navbar';
+import Header from './components/Header';
 import Home from './components/Home';
 import About from "./components/About";
 import Projects from './components/Projects';
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <div className="App">
-      <Navbar />
+      <Header />
       <main>
         <Home />
         <About />
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,43 +1,40 @@
 import React, { useEffect, useState } from 'react';
 
-const Navbar = () => {
+const Header = () => {
   const [offset, setOffset] = useState(0)
-  const [navTop, setNavTop] = useState("0")
-  const [navShadow, setNavShadow] = useState("none")
-  const [navHeight, setNavHeight] = useState("5rem")
-  const [mobileNav, setMobileNav] = useState(false)
+  const [headerTop, setHeaderTop] = useState("0")
+  const [headerShadow, setHeaderShadow] = useState("none")
+  const [headerHeight, setHeaderHeight] = useState("5rem")
+  const [toggleNav, setNavToggle] = useState(false)
 
   useEffect(() => {
     window.onscroll = () => {
       const scrollUp = window.pageYOffset
       if(scrollUp > offset) {
-        setNavTop("-5rem")
+        setHeaderTop("-5rem")
       } else {
-        setNavTop("0")
+        setHeaderTop("0")
       }
       setOffset(scrollUp)
     }
 
     if(offset > 0) {
-      setNavShadow("0 20px 20px rgba(0,0,0,0.05)")
-      setNavHeight("4rem")
+      setHeaderShadow("0 20px 20px rgba(0,0,0,0.05)")
+      setHeaderHeight("4.5rem")
     } else {
-      setNavShadow("none")
-      setNavHeight("5rem")
+      setHeaderShadow("none")
+      setHeaderHeight("5rem")
     }
 
   }, [offset]);
 
   return (
-    <nav id="nav" style={{ top: navTop, boxShadow: navShadow, height: navHeight }}>
+    <header id="nav" style={{ top: headerTop, boxShadow: headerShadow, height: headerHeight }}>
       <h1>
         <a href="#" id="logo">jOHN</a>
       </h1>
-       <div className="right-nav">
-        <ul>
-          <li>
-            <a href="#home">Home</a>
-          </li>
+      <nav>
+        <ul className="primary-nav">
           <li>
             <a href="#about">About</a>
           </li>
@@ -50,16 +47,17 @@ const Navbar = () => {
           <li>
             <a href="#contact">Contact</a>
           </li>
+          <button>R&eacute;sum&eacute;</button>
         </ul>
-      </div>
+      </nav>
       <button>Let's Connect</button>
-      <div className={mobileNav ? "menu-btn close" : "menu-btn"} onClick={() => setMobileNav(!mobileNav)}>
+      <div className={toggleNav ? "menu-btn close" : "menu-btn"} onClick={() => setNavToggle(!toggleNav)}>
         <div className="btn-line" />
         <div className="btn-line" />
         <div className="btn-line" />
       </div>
-    </nav>
+    </header>
   )
 }
 
-export default Navbar;
+export default Header;
