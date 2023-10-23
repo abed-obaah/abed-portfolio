dc95e9c86913cc663128b61bd5959863c3210393
John Adibe <johnadibe450@gmail.com>
2023-10-24 00:57:52 +0100

    about section

--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -1,17 +1,12 @@
-import classNames from "classnames";
-
-const SectionTitle = ({ title, size }) => {
-  const content = title.toLowerCase().replace(" ", "_");
+const SectionTitle = ({ title }) => {
+  const content = title.toLowerCase();
 
   return (
-    <h2
-      id="section-title"
-      className={classNames(
-        `font-bold text-3xl whitespace-nowrap before:content-['${content}']`,
-        { "!text-5xl": size === "lg" }
-      )}
-    >
+    <h2 className="relative font-bold text-4xl mt-3 whitespace-nowrap">
       {title}
+      <span className="absolute text-5xl font-bold left-0 md:left-1/2 md:-translate-x-1/2 -top-6 bg-clip-text text-transparent bg-gradient-to-b from-white/[.15] to-white/0">
+        {content}
+      </span>
     </h2>
   );
 };
--- src/pages/home/components/About.tsx
+++ src/pages/home/components/About.tsx
@@ -1,39 +1,35 @@
-import gsap from "gsap";
-import SplitType from "split-type";
-import { useEffect, useRef } from "react";
+import { useRef } from "react";
 import SectionTitle from "@/components/SectionTitle";
 
 const About = () => {
   const textRef = useRef<HTMLParagraphElement>(null);
 
-  useEffect(() => {
-    const splitText = new SplitType(textRef.current, { types: "chars, words" });
+  // useEffect(() => {
+  //   const splitText = new SplitType(textRef.current, { types: "chars, words" });
 
-    gsap.from(splitText.chars, {
-      scrollTrigger: {
-        trigger: textRef.current,
-        start: "top 80%",
-        end: "top 20%",
-        scrub: 1,
-        markers: false,
-      },
-      color: "rgb(247, 254, 231, .25)",
-      stagger: 0.1,
-    });
-  }, []);
+  //   gsap.from(splitText.chars, {
+  //     scrollTrigger: {
+  //       trigger: textRef.current,
+  //       start: "top 80%",
+  //       end: "top 20%",
+  //       scrub: 1,
+  //       markers: false,
+  //     },
+  //     color: "rgb(255, 255, 255, .5)",
+  //     stagger: 0.1,
+  //   });
+  // }, []);
 
   return (
     <section id="about" className="container">
-      <div className="flex gap-6">
-        <SectionTitle title="About Me" size="lg" />
-        <div className="flex-1 p-8 border border-[#1a1a1a] border-opacity-70">
-          <p ref={textRef} className={`text-lime-50 text-[calc(1vw_+_1vh)]`}>
-            Enthusiastic frontend developer with over 3 years of experience
-            building responsive and scalable web applications, with a particular
-            passion for developing innovative UI/UX solutions. I look forward to
-            applying this experience in my next role.
-          </p>
-        </div>
+      <div className="flex flex-col md:flex-row justify-between gap-7 sm:gap-10 bg-white/[.025] p-7 sm:p-14 md:px-20 md:py-14 border">
+        <SectionTitle title="About Me" />
+        <p ref={textRef} className="text-white/80 md:w-9/12 text-lg sm:text-xl">
+          Enthusiastic frontend developer with over 3 years of experience
+          building responsive and scalable web applications, with a particular
+          passion for developing innovative UI/UX solutions. I look forward to
+          applying this experience in my next role.
+        </p>
       </div>
     </section>
   );
